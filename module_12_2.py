6da65f94d9ef79182785e8adaa6c11906086d84b
SirEvgen <sandrofa6666666@gmail.com>
2024-10-06 14:10:44 +0300

    Update module_12_2.py

--- module_12_2.py
+++ module_12_2.py
@@ -11,26 +11,34 @@ class TournamentTest(unittest.TestCase):
         self.runner1 = Runner('Усейн', 10)
         self.runner2 = Runner('Андрей', 9)
         self.runner3 = Runner('Ник', 3)
-
-    def test_run(self):
         tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
-        result = tournament.start()
+        self.result = tournament.start()
         result_name = []
         place = 0
-        expected_result = {}
-        expected_finishers = 3
-        for runner in result.values():
+        self.expected_result = {}
+        self.expected_finishers = 3
+
+        for runner in self.result.values():
             result_name.append(runner)
+
             place += 1
-            expected_result[place] = runner.name
-        self.assertEqual(expected_result, result, False)
-        self.assertTrue(expected_result == result, True)
-        self.assertEqual(len(result.values()), expected_finishers)
-        self.assertEqual(expected_result, result)
+            self.expected_result[place] = runner.name
+
+    def test_run(self):
+        self.assertEqual(self.expected_result, self.result)
         """Можно было конечно перебрать все значения в словаре и с помощью метода __str__
         из класса Runner заменить их на читаемые, но получается громоздкий код, да и лишняя
         работа ни к чему"""
-        self.all_results.update(expected_result)
+        self.all_results.update(self.expected_result)
+
+    def test_inequality(self):
+        self.assertEqual(self.expected_result, self.result, False)
+
+    def test_equality(self):
+        self.assertTrue(self.expected_result == self.result, True)
+
+    def test_finishers(self):
+        self.assertEqual(len(self.result.values()), self.expected_finishers)
 
     @classmethod
     def tearDownClass(cls):
